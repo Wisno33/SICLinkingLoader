844d26de9f70d787be8c90a85b882219a09b027d
David Wisnosky <DJWisnosky@icloud.com>
2021-04-11 18:41:35 -0400

    Initial Commit

---
+++ SICLinkingLoader/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SICLinkingLoader
+//
+//  Created by David Wisnosky on 4/11/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
